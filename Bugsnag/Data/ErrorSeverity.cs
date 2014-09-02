3c8371a6cb3a5161a938274f95257e3a588cd708
Mart Roosmaa <roosmaa@gmail.com>
2014-09-02 12:26:01 +0300

    Organize usings in Bugsnag project.

--- Bugsnag/Data/ErrorSeverity.cs
+++ Bugsnag/Data/ErrorSeverity.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Bugsnag.Data
 {
     public enum ErrorSeverity
--- Bugsnag/Data/Event.cs
+++ Bugsnag/Data/Event.cs
@@ -1,7 +1,6 @@
-using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Bugsnag.Json;
-using System.Collections.Generic;
 
 namespace Bugsnag.Data
 {
