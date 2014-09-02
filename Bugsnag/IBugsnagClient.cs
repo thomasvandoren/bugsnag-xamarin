59a1fafff4889214b5afe28b9a24258e07edaa27
Mart Roosmaa <roosmaa@gmail.com>
2014-09-02 12:18:09 +0300

    Introduce IBugsnagClient to shared library.

---
+++ Bugsnag/IBugsnagClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Bugsnag.Data;
+
+namespace Bugsnag
+{
+    interface IBugsnagClient : IDisposable
+    {
+        bool AutoNotify { get; set; }
+
+        string Context { get; set; }
+
+        string ReleaseStage { get; set; }
+
+        List<string> NotifyReleaseStages { get; set; }
+
+        List<string> Filters { get; set; }
+
+        List<Type> IgnoredExceptions { get; set; }
+
+        List<string> ProjectNamespaces { get; set; }
+
+        string UserId { get; set; }
+
+        string UserEmail { get; set; }
+
+        string UserName { get; set; }
+
+        void SetUser (string id, string email = null, string name = null);
+
+        void AddToTab (string tabName, string key, object value);
+
+        void ClearTab (string tabName);
+
+        void TrackUser ();
+
+        void Notify (Exception e, ErrorSeverity severity = ErrorSeverity.Error, Metadata extraMetadata = null);
+    }
+}
